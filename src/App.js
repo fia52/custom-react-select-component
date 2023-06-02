a990c2df5fa2fb3e713febc86e68bb25e91260cc
i_frolov <i_frolov@aerocomposit.ru>
2023-06-02 12:06:22 +0300

    reformation

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+
 import "./styles.css";
-import Select from "react-select";
-import CustomSelect from "./components/input/CustomSelect";
+import CustomSelect from "./CustomSelect";
 
 export default function App() {
-  // React state to manage selected options
-  const [selectedOptions, setSelectedOptions] = useState();
-
-  // Array of all options
-  const optionList = [
-    { value: "red", label: "Red" },
-    { value: "green", label: "Green" },
-    { value: "yellow", label: "Yellow" },
-    { value: "blue", label: "Blue" },
-    { value: "white", label: "White" },
-  ];
-
-  // Function triggered on selection
-  function handleSelect(data) {
-    setSelectedOptions(data);
-  }
   return (
     <div className="app">
-      {/*<h2>Choose your color</h2>*/}
-      {/*<div className="dropdown-container">*/}
-      {/*  <Select*/}
-      {/*    options={optionList}*/}
-      {/*    placeholder="Select color"*/}
-      {/*    value={selectedOptions}*/}
-      {/*    onChange={handleSelect}*/}
-      {/*    isSearchable={true}*/}
-      {/*    isMulti*/}
-      {/*  />*/}
-      {/*</div>*/}
-
-      <div className="search-bar-container">
             <CustomSelect />
-        </div>
-
     </div>
   );
 }
